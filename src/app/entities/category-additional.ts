37e654203af90ebe5eec44f24aa2b0d2b517706a
Alexandre Gonçalves <agoncalves@ekan.com.br>
2023-03-24 16:14:50 -0300

    create category-additional entity

---
+++ src/app/entities/category-additional.ts
@@ -0,0 +1,35 @@
+import { Replace } from '@helpers/Replace';
+import { randomUUID } from 'node:crypto';
+import { BaseEntity } from './base-entity';
+
+export interface CategoryAdditionalProps {
+  name: string;
+  order: number;
+}
+
+export class CategoryAdditional extends BaseEntity {
+  private props: CategoryAdditionalProps;
+
+  constructor(
+    props: Replace<CategoryAdditionalProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    super({ id, createdAt: props.createdAt });
+    this.props = props;
+  }
+
+  public set name(name: string) {
+    this.props.name = name;
+  }
+
+  public get name(): string {
+    return this.props.name;
+  }
+  public set order(order: number) {
+    this.props.order = order;
+  }
+
+  public get order(): number {
+    return this.props.order;
+  }
+}
