890b652c5671724e4bb03e00b39af5521d2b7482
Alexandre Queiroz <alexandre.goncalves33@outlook.com>
2023-08-29 12:48:29 -0300

    remove swagger

--- src/shared/modules/auth/dtos/login-admin-auth-body.ts
+++ src/shared/modules/auth/dtos/login-admin-auth-body.ts
@@ -1,18 +1,11 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class LoginAdminAuthBody {
-  @ApiProperty({
-    description: 'O email do usuário',
-    type: String,
-  })
+
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty({
-    description: 'A senha do usuário',
-    type: String,
-  })
+
   @IsNotEmpty()
   password: string;
 }
