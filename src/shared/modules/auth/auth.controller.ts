066db71db0cb4c4b61e38460dc963ce771d9a7ed
Alexandre Queiroz <alexandre.goncalves33@outlook.com>
2023-08-31 18:49:43 -0300

    login

--- src/shared/modules/auth/auth.controller.ts
+++ src/shared/modules/auth/auth.controller.ts
@@ -1,46 +1,23 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
-import { LoginAdminAuthBody } from './dtos/login-admin-auth-body';
+import { LoginAuthBody } from './dtos/login-auth-body';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
   @Post('login')
-  async signIn(@Body() signInDto: LoginAdminAuthBody) {
+  async signIn(@Body() signInDto: LoginAuthBody) {
     return await this.authService.signIn(
-      signInDto.email,
-      signInDto.password,
-      // signInDto.restaurantId,
+      signInDto,
     );
   }
-
-  @HttpCode(HttpStatus.OK)
-  @Post('login/admin')
-  async signInAdmin(@Body() signInDto: LoginAdminAuthBody) {
-    return await this.authService.signInAdmin(
-      signInDto.email,
-      signInDto.password,
-    );
-  }
-
-  // @HttpCode(HttpStatus.OK)
-  // @Post('login/google')
-  // @ApiBody({ type: LoginAuthBody })
-  // @ApiConsumes('application/x-www-form-urlencoded')
-  // async signInGoogle(@Body() signInDto: LoginGoogleBody) {
-  //   return await this.authService.signInGoogle(
-  //     signInDto.email,
-  //     signInDto.googleId,
-  //   );
-  // }
 
   @UseGuards(AuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+  getProfile(@Request() req: Request) {
+    return req['user'];
   }
 }
